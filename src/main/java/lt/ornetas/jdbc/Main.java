07d504f1f8b602a46968449ff397121627896b5c
orentas <o.b.ornetas@gmail.com>
2023-01-12 17:06:24 +0200

    First conection to mylocalhost with jdbc

---
+++ src/main/java/lt/ornetas/jdbc/Main.java
@@ -0,0 +1,21 @@
+package lt.ornetas.jdbc;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/classicmodels",
+                    "root",
+                    "root");
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from customers");
+
+            while (rs.next()){
+            System.out.println(rs.getInt(1) + " | " + rs.getString("customerName"));}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
