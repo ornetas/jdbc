0bab7ff96a022ba5561fd224fc9ef06be0113549
orentas <o.b.ornetas@gmail.com>
2023-01-13 14:06:06 +0200

    Add and implement CustomerService.java

--- src/main/java/lt/ornetas/jdbc/Main.java
+++ src/main/java/lt/ornetas/jdbc/Main.java
@@ -7,9 +7,8 @@ public class Main {
     public static void main(String[] args) {
 
         CustomerService customerService = new CustomerService();
-        List<Customer> customers = new ArrayList<>();
 
-        customers = customerService.getAllCustomers();
+        List<Customer> customers = customerService.getAllCustomers();
         customers.forEach(System.out::println);
 
     }
